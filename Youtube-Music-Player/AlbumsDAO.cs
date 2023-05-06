421aaf96d17f72c39e711991617ea7d3edb4f0a9
ayyad2k2 <ayyad2k2@gmail.com>
2023-05-06 22:07:46 +0800

    App connected into database
    using SQL  dependancies,
    using SQL datasource with password and stuff,
    using Mysqlreader code,
    using while(reader.read())

--- Youtube-Music-Player/AlbumsDAO.cs
+++ Youtube-Music-Player/AlbumsDAO.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +11,38 @@ namespace CheapSpotify
 {
     internal class AlbumsDAO
     {
-        // V1 only contains fake data. No connection to actual db
-        public List<Album> albums = new List<Album>();
+        string connectionString = "datasource=localhost;port=3306;username=Albums;password=albums;database=albums;";
+        public List<Album> getAllAlbums()
+        {
+            List<Album> returnThese = new List<Album>();
+
+            MySqlConnection connection = new MySqlConnection 
+                (connectionString);
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM albums",
+                connection);
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album a = new Album()
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        ImageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+                    returnThese.Add(a);
+                }
+            }
+                connection.Close();
+
+            return returnThese;
+        }
+
+
     }
 }
--- Youtube-Music-Player/Form1.cs
+++ Youtube-Music-Player/Form1.cs
@@ -13,35 +13,10 @@ namespace CheapSpotify
         private void loadAlbumsBtn_Click(object sender, EventArgs e)
         {
             AlbumsDAO albumsDAO = new AlbumsDAO();
-            Album a1 = new Album()
-            {
-                ID = 1,
-                AlbumName = "Smithereens",
-                ArtistName = "Joji",
-                Year = 2023,
-                ImageURL = "https://www.youtube.com",
-                Description = "WOW this is a description"
+            
+            albumBindingSource.DataSource = albumsDAO.getAllAlbums();
 
-            };
-
-            Album a2 = new Album()
-            {
-                ID = 1,
-                AlbumName = "Nectar",
-                ArtistName = "Joji",
-                Year = 2023,
-                ImageURL = "https://www.youtube.com",
-                Description = "WOW this is a description"
-
-            };
-
-            albumsDAO.albums.Add(a1);
-            albumsDAO.albums.Add(a2);
-
-            // connect the lsit to the grid view control 
-            albumBindingSource.DataSource = albumsDAO.albums;
-
-            dataGridView1.DataSource = albumBindingSource; 
+            dataGridView1.DataSource = albumBindingSource;
 
         }
     }
